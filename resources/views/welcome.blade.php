8dbac26f3740fb2fb564fa1b0fc5fd6b34063687
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2023-10-06 16:46:53 -0500

    change google manager

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,15 @@
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-TMBFMLJN');</script>
     <!-- End Google Tag Manager -->
+
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VMQWGCQ4T9"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MPVD2FP50D"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-VMQWGCQ4T9');
+        gtag('config', 'G-MPVD2FP50D');
     </script>
 	<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
